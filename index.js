2dcdd656baf4aaeffc7c41a558f81b42c076f1b6
Yuxiang Li <seanliyuxiang@gmail.com>
2021-08-31 17:41:15 -0500

    commit tree b1144a55dc6d08d6e424abb47d067f11aee371be

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+// for some reason, doing it function expression style didn't work
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'jon';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'tracy';
+}
